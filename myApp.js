6ef9fc3aa9e23d4c950742395c692ffccd9164e0
Sihle <69849355+uniStu@users.noreply.github.com>
2023-12-05 03:28:27 -0500

    Update myApp.js

--- myApp.js
+++ myApp.js
@@ -1,7 +1,7 @@
 const express = require('express');
 const helmet = require('helmet');
 const bcrypt = require('bcrypt');
-const PORT = process.env.PORT || 3690;
+const PORT = process.env.PORT || 3000;
 
 const app = express();
 
@@ -87,8 +87,9 @@ const result = bcrypt.compareSync(myPlaintextPassword, hash);
 
 // ...other middleware and routes
 
+const PORT = process.env.PORT || 3000;
 app.listen(PORT, () => {
-  console.log(`server started on port ${PORT}`);
+  console.log(`Server is running on port ${PORT}`);
 });
 
 
