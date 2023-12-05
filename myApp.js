23fbe3dcc652db2a9622b56f1afed1810d12dab2
Sihle <69849355+uniStu@users.noreply.github.com>
2023-12-05 04:09:11 -0500

    Update myApp.js

--- myApp.js
+++ myApp.js
@@ -1,5 +1,76 @@
 const express = require('express');
 const app = express();
+const helmet = require('helmet');
+const bcrypt = require('bcrypt');
+
+// Install and require Helmet
+app.use(helmet());
+
+// Hide potentially dangerous information
+app.use(helmet.hidePoweredBy());
+
+// Mitigate the risk of clickjacking
+app.use(helmet.frameguard({ action: 'deny' }));
+
+// Mitigate the risk of XSS attacks
+app.use(helmet.xssFilter());
+
+// Avoid inferring the response MIME type
+app.use(helmet.noSniff());
+
+// Prevent IE from opening untrusted HTML
+app.use(helmet.ieNoOpen());
+
+// Ask browsers to access your site via HTTPS only (with a max age of 90 days)
+app.use(
+  helmet.hsts({
+    maxAge: 90 * 24 * 60 * 60, // 90 days
+    includeSubDomains: true,
+    preload: true,
+  })
+);
+
+// Disable DNS prefetching
+app.use(helmet.dnsPrefetchControl());
+
+// Disable client-side caching
+app.use(helmet.noCache());
+
+// Set a Content Security Policy
+app.use(
+  helmet.contentSecurityPolicy({
+    directives: {
+      defaultSrc: ["'self'"],
+      styleSrc: ["'self'", 'maxcdn.bootstrapcdn.com'],
+      scriptSrc: ["'self'", 'code.jquery.com'],
+    },
+  })
+);
+
+// Understand BCrypt hashes
+const saltRounds = 10;
+const myPlaintextPassword = 's0/\/\P4$$w0rD';
+bcrypt.genSalt(saltRounds, (err, salt) => {
+  bcrypt.hash(myPlaintextPassword, salt, (err, hash) => {
+    // Store hash in your password DB.
+    bcrypt.compare(myPlaintextPassword, hash, (err, result) => {
+      // result == true
+    });
+  });
+});
+
+// Hash and compare passwords asynchronously
+async function hashAndComparePasswords() {
+  const salt = await bcrypt.genSalt(saltRounds);
+  const hash = await bcrypt.hash(myPlaintextPassword, salt);
+  const result = await bcrypt.compare(myPlaintextPassword, hash);
+  // result == true
+}
+
+// Hash and compare passwords synchronously
+const hash = bcrypt.hashSync(myPlaintextPassword, saltRounds);
+const result = bcrypt.compareSync(myPlaintextPassword, hash);
+// result == true
 
 
 
