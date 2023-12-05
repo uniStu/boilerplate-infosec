46e2e217fc644bbd4a00f052523f60700f1f1b1b
Sihle <69849355+uniStu@users.noreply.github.com>
2023-12-05 03:46:52 -0500

    Update myApp.js

--- myApp.js
+++ myApp.js
@@ -1,7 +1,7 @@
 const express = require('express');
 const helmet = require('helmet');
 const bcrypt = require('bcrypt');
-const PORT = process.env.PORT || 3030;
+const PORT = process.env.PORT || 3070;
 
 const app = express();
 
