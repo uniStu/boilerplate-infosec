df2052be343b7461cb12c6b7f24862a43429dba1
Sihle <69849355+uniStu@users.noreply.github.com>
2023-12-05 02:16:48 -0500

    Update myApp.js

--- myApp.js
+++ myApp.js
@@ -1,71 +1,14 @@
 const express = require('express');
 const helmet = require('helmet');
+
 const app = express();
 
-const PORT = process.env.PORT || 3030;
+// Use the helmet.hidePoweredBy() middleware to remove X-Powered-By header
+app.use(helmet.hidePoweredBy());
 
-// your code
+// ...other middleware and routes
 
-app.listen(PORT, () => {
-  console.log(`server started on port ${PORT}`);
-});
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-module.exports = app;
-const api = require('./server.js');
-app.use(express.static('public'));
-app.disable('strict-transport-security');
-app.use('/_api', api);
-app.get("/", function (request, response) {
-  response.sendFile(__dirname + '/views/index.html');
-});
-let port = process.env.PORT || 3000;
-app.listen(port, () => {
-  console.log(`Your app is listening on port ${port}`);
+// Start the server
+app.listen(3000, () => {
+  console.log('Server is running on port 3000');
 });
